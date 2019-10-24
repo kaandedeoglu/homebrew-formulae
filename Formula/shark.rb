0b091a0ad6ca3cee327212940ac176600c513a23
Kaan Dedeoglu <kaan.dedeoglu@joincoup.com>
2019-10-24 14:38:09 +0200

    Bump version to 1.1.2

--- Formula/shark.rb
+++ Formula/shark.rb
@@ -2,7 +2,7 @@ class Shark < Formula
   desc "Swift CLI for strong-typing images, colors and localizations"
   homepage "https://github.com/kaandedeoglu/Shark"
   url "https://github.com/kaandedeoglu/Shark.git",
-      :tag => "1.1.1"
+      :tag => "1.1.2"
   head "https://github.com/kaandedeoglu/Shark.git"
 
   depends_on :xcode => ["10.2", :build]
