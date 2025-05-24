94667e8cd1d762615e7f4ad0f0455f56da3942d3
Dr. Michael Lauer <mickey@vanille-media.de>
2025-05-24 13:59:57 +0200

    Bump to version 1.8

--- Formula/shark.rb
+++ Formula/shark.rb
@@ -1,9 +1,9 @@
 class Shark < Formula
-  desc "Swift CLI for strong-typing images, colors and localizations"
+  desc "Swift CLI for strong-typing images, fonts, colors, localizations, and more."
   homepage "https://github.com/kaandedeoglu/Shark"
   url "https://github.com/kaandedeoglu/Shark.git"
-  head "https://github.com/kaandedeoglu/Shark.git", :tag => "1.7"
-  version "1.7"
+  head "https://github.com/kaandedeoglu/Shark.git", :tag => "1.8"
+  version "1.8"
 
   depends_on :xcode => ["11.0", :build]
 
