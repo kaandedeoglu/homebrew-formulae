e9a206c1e40d2efefdb1fb9064bbaf4deee10432
Dr. Michael Lauer <mickey@vanille-media.de>
2026-05-05 11:59:34 +0200

    bump Shark to 1.8.4

--- Formula/shark.rb
+++ Formula/shark.rb
@@ -2,8 +2,8 @@ class Shark < Formula
   desc "Swift CLI for strong-typing images, fonts, colors, localizations, and more."
   homepage "https://github.com/kaandedeoglu/Shark"
   url "https://github.com/kaandedeoglu/Shark.git"
-  head "https://github.com/kaandedeoglu/Shark.git", :tag => "1.8.3"
-  version "1.8.3"
+  head "https://github.com/kaandedeoglu/Shark.git", :tag => "1.8.4"
+  version "1.8.4"
 
   depends_on :xcode => ["11.0", :build]
 
