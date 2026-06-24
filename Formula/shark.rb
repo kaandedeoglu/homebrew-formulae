d447110781de6da17ae2b0abcc5d445f012ff4bf
Dr. Michael Lauer <mickey@vanille-media.de>
2026-06-24 13:07:00 +0200

    Update Shark formula to 1.8.7

--- Formula/shark.rb
+++ Formula/shark.rb
@@ -2,14 +2,14 @@ class Shark < Formula
   desc "Swift CLI for strong-typing images, fonts, colors, localizations, and more"
   homepage "https://github.com/kaandedeoglu/Shark"
   url "https://github.com/kaandedeoglu/Shark.git",
-      tag:      "1.8.6",
-      revision: "dce6effd9929c4a0c2772c62c90b0d5d8921b90c"
+      tag:      "1.8.7",
+      revision: "4f5e20238957abb919c825380a9bf17965d7228b"
   head "https://github.com/kaandedeoglu/Shark.git", branch: "master"
 
   depends_on xcode: ["11.0", :build]
 
   def install
-    system "swift", "build", "-c", "release", "--disable-sandbox"
+    system "swift", "build", "--build-system", "native", "-c", "release", "--disable-sandbox"
     bin.install ".build/release/Shark" => "shark"
   end
 
