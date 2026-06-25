06edd4ee8ca44edc3227822c0b45517f280926a9
Dr. Michael Lauer <mickey@vanille-media.de>
2026-06-25 16:43:09 +0200

    Release Shark 2.0 and keep Shark 1 formula

--- Formula/shark@1.rb
+++ Formula/shark@1.rb
@@ -1,19 +1,20 @@
-class Shark < Formula
+class SharkAT1 < Formula
   desc "Swift CLI for strong-typing images, fonts, colors, localizations, and more"
   homepage "https://github.com/kaandedeoglu/Shark"
   url "https://github.com/kaandedeoglu/Shark.git",
-      tag:      "2.0",
-      revision: "1dd192fc8131f9e0fe2a111cfedf10a9ddede437"
-  head "https://github.com/kaandedeoglu/Shark.git", branch: "master"
+      tag:      "1.8.7",
+      revision: "4f5e20238957abb919c825380a9bf17965d7228b"
 
   depends_on xcode: ["15.3", :build]
 
   def install
     system "swift", "build", "--build-system", "native", "-c", "release", "--disable-sandbox"
-    bin.install ".build/release/Shark" => "shark"
+    bin.install ".build/release/Shark" => "shark1"
+    bin.install_symlink "shark1" => "shark-1"
   end
 
   test do
-    system "#{bin}/shark", "--help"
+    system "#{bin}/shark1", "--help"
+    system "#{bin}/shark-1", "--help"
   end
 end
